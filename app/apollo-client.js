fa00e4e40a5d5c2dd9fb455578203c8601959bc5
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-06-05 14:55:40 -0400

    Added topic route for dynamic page routing example

--- app/apollo-client.js
+++ app/apollo-client.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+// Add dev tools
 
 const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT,
     cache: new InMemoryCache(),
+    connectToDevTools: true
 });
 
 export default client;
---
+++ pages/topics/[slug].js
@@ -0,0 +1,76 @@
+import { gql } from "@apollo/client";
+import client from "../../app/apollo-client";
+import { useEffect } from "react";
+
+// Get the tag by slug from the wordpress graphql api
+export async function getStaticProps({ params }) {
+  const { data } = await client.query({
+    query: gql`
+      query Tag($slug: ID!) {
+        tag(id: $slug, idType: SLUG) {
+          name
+          posts {
+            nodes {
+              title
+              excerpt
+              slug
+              id
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      slug: params.slug,
+    },
+  });
+
+  return {
+    props: {
+      tag: data.tag,
+    },
+  };
+}
+
+// getStaticPaths
+export async function getStaticPaths() {
+  const { data } = await client.query({
+    query: gql`
+      query Tags {
+        tags {
+          nodes {
+            slug
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    paths: data.tags.nodes.map((tag) => `/topics/${tag.slug}`) || [],
+    fallback: false,
+  };
+}
+
+// Display the tag name and posts
+export default function Tag({ tag }) {
+
+  useEffect(() => {
+    console.log(tag);
+  }, [tag]);
+  // If the tag doesn't exist in WP, show "doesn't exist"
+  if (!tag) return <div>doesn't exist</div>;
+  return (
+    <>
+      <h1>Topic: {tag.name}</h1>
+      <div className="posts">
+        {tag.posts.nodes.map((post) => (
+          <div key={post.id} className="post">
+            <h2>{post.title}</h2>
+            <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
