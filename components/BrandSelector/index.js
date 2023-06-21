fd1ac42cfe2472b71db42b09d48c2fb03f49c792
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-06-21 09:54:44 -0400

    Source URL for brand logos

--- components/BrandSelector/index.js
+++ components/BrandSelector/index.js
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 import { Button, Modal, Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import { useQuery } from "@apollo/client";
 import client from "@/app/apollo-client";
-import GET_BRANDS  from "@/app/graphql/queries/GET_BRANDS";
+import GET_BRANDS from "@/app/graphql/queries/GET_BRANDS";
 import styles from "./BrandSelector.module.css";
 const BrandSelector = () => {
   const { loading, error, data } = useQuery(GET_BRANDS, {
     client: client,
   });
 
-
   useEffect(() => {
     if (data) {
-      console.log(data);
+      console.log("brands", data);
     }
   }, [data]);
   if (loading) return <p>Loading...</p>;
@@ -21,52 +20,53 @@ const BrandSelector = () => {
   if (!data) return null;
   return (
     <>
-    <Container>
-      <Row>
-        <Col>
-               <h4 class="centered">Check out our complete portfolio of iconic brands</h4>
+      <Container>
+        <Row>
+          <Col>
+            <h4 class="centered">
+              Check out our complete portfolio of iconic brands
+            </h4>
 
-          <Tabs defaultActiveKey="all" id="brands">
-            <Tab eventKey="all" title="All">
-            </Tab>
-            <Tab eventKey="soups" title="Soups, Meals & Sauces">
-            </Tab>
-            <Tab eventKey="snacks" title="Snacks & Beverages">
-            </Tab>
-          </Tabs> 
+            <Tabs defaultActiveKey="all" id="brands">
+              <Tab eventKey="all" title="All"></Tab>
+              <Tab eventKey="soups" title="Soups, Meals & Sauces"></Tab>
+              <Tab eventKey="snacks" title="Snacks & Beverages"></Tab>
+            </Tabs>
+          </Col>
+        </Row>
+      </Container>
 
-        </Col>
-      </Row>
-     </Container>
-      
-        <Container>
-          <Row>
-
-            {
-              (data.brands.nodes.length > 0) &&
-            data.brands.nodes.map((brand) => (
-            
-            <Col key={brand.id} className={styles.w20}>
-               {/* <a href={`/brands/${brand.slug}`}>{brand.title}</a> */}
-              <a href="#" data-toggle="modal" data-target="#exampleModalCenter">
-                {brand.title}
-                
-                
-                <img src={brand.featuredImage.node.guid} className="img-fluid" />
-               {/* why doesnt this work? 
-               
-                <img src={brand.brandFields.brandLogo.sourceUrl} className="img-fluid" />
-                */}
-    
-
-               </a> </Col>
-            ))}
-
-
-          </Row>
-          </Container>
+      <Container>
+        <Row>
+          {data.brands.nodes.length > 0 &&
+            data.brands.nodes.map((brand) => <Brand brand={brand} key={brand.id} />)}
+        </Row>
+      </Container>
     </>
   );
-}
+};
+
+const Brand = ({ brand }) => {
+  const [logoUrl, setLogoUrl] = useState(brand.brandFields.brandLogo);
+  
+  useEffect(() => {
+    if (brand.brandFields.brandLogo) {
+      setLogoUrl(brand.brandFields.brandLogo.sourceUrl);
+    }
+  }, [brand]);
+  return (
+    <Col className={styles.w20}>
+      {/* <a href={`/brands/${brand.slug}`}>{brand.title}</a> */}
+      <a href="#" data-toggle="modal" data-target="#exampleModalCenter">
+        {brand.title}
+
+        <img src={brand.featuredImage.node.guid} className="img-fluid" />
+
+        <img src={logoUrl} className="img-fluid" />
+
+      </a>
+    </Col>
+  );
+};
 
 export default BrandSelector;
