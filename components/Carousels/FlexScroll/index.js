86561c66398959e0de78760b7c7c5ee04075c48a
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-06-22 14:43:14 -0400

    Updates to search

--- components/Carousels/FlexScroll/index.js
+++ components/Carousels/FlexScroll/index.js
@@ -1,24 +1,27 @@
 import styles from "./flexscroll.module.css";
-export function Resource({ link, thumb, cardTag, alttext, doctitle, linkCopy }) {
+export function Resource({
+  link,
+  thumb,
+  cardTag,
+  alttext,
+  doctitle,
+  linkCopy,
+}) {
   return (
     <>
-      <li className={(styles.card)}>
-
+      <li className={styles.card}>
         <div>
-          <div variant="primary" className={(styles.cardTag)}>{(cardTag)}</div>
-          <img
-            className={styles.cardImage}
-            src={(thumb)}
-            alt={(alttext)}
-          />
-          <div className={(styles.cardContent)}>
-            {(doctitle)}
+          <div variant="primary" className={styles.cardTag}>
+            {cardTag}
           </div>
+          <img className={styles.cardImage} src={thumb} alt={alttext} />
+          <div className={styles.cardContent}>{doctitle}</div>
 
-          <div className={(styles.cardLinkWrapper)}>
-            <a href="" className={(styles.cardLink)}>{(linkCopy)}</a>
+          <div className={styles.cardLinkWrapper}>
+            <a href="" className={styles.cardLink}>
+              {linkCopy}
+            </a>
           </div>
-
         </div>
       </li>
     </>
@@ -28,61 +31,61 @@ export function Resource({ link, thumb, cardTag, alttext, doctitle, linkCopy }) 
 const FlexScroll = () => {
   return (
     <>
-   <div className={(styles.cardsWrapper)}>
-          <ul className={(styles.cards)}>
-            <Resource
-              link={'https://google.com'}
-              cardTag={'Sell Sheet'}
-              thumb={'https://via.placeholder.com/300x300'}
-              alttext={'some alt text'}
-              doctitle={'Sell Sheet Title'}
-              linkCopy={'Download'}
-            />     
-              <Resource
-              link={'https://google.com'}
-              cardTag={'NEW'}
-              thumb={'https://via.placeholder.com/300x300'}
-              alttext={'some alt text'}
-              doctitle={'Sell Sheet Title'}
-              linkCopy={'Download'}
-            />     
-              <Resource
-              link={'https://google.com'}
-              cardTag={'tag'}
-              thumb={'https://via.placeholder.com/300x300'}
-              alttext={'some alt text'}
-              doctitle={'Sell Sheet Title'}
-              linkCopy={'Download'}
-            />     
-              <Resource
-              link={'https://google.com'}
-              cardTag={'NEW'}
-              thumb={'https://via.placeholder.com/300x300'}
-              alttext={'some alt text'}
-              doctitle={'Sell Sheet Title'}
-              linkCopy={'Download'}
-            />     
-              <Resource
-              link={'https://google.com'}
-              cardTag={'NEW'}
-              thumb={'https://via.placeholder.com/300x300'}
-              alttext={'some alt text'}
-              doctitle={'Sell Sheet Title'}
-              linkCopy={'Download'}
-            />     
-               
-              <Resource
-              link={'https://google.com'}
-              cardTag={'NEW'}
-              thumb={'https://via.placeholder.com/300x300'}
-              alttext={'some alt text'}
-              doctitle={'Sell Sheet Title'}
-              linkCopy={'Download'}
-            />     
-    </ul>
-    </div>
+      <div className={styles.cardsWrapper}>
+        <ul className={styles.cards}>
+          <Resource
+            link={"https://google.com"}
+            cardTag={"Sell Sheet"}
+            thumb={"https://via.placeholder.com/300x300"}
+            alttext={"some alt text"}
+            doctitle={"Sell Sheet Title"}
+            linkCopy={"Download"}
+          />
+          <Resource
+            link={"https://google.com"}
+            cardTag={"NEW"}
+            thumb={"https://via.placeholder.com/300x300"}
+            alttext={"some alt text"}
+            doctitle={"Sell Sheet Title"}
+            linkCopy={"Download"}
+          />
+          <Resource
+            link={"https://google.com"}
+            cardTag={"tag"}
+            thumb={"https://via.placeholder.com/300x300"}
+            alttext={"some alt text"}
+            doctitle={"Sell Sheet Title"}
+            linkCopy={"Download"}
+          />
+          <Resource
+            link={"https://google.com"}
+            cardTag={"NEW"}
+            thumb={"https://via.placeholder.com/300x300"}
+            alttext={"some alt text"}
+            doctitle={"Sell Sheet Title"}
+            linkCopy={"Download"}
+          />
+          <Resource
+            link={"https://google.com"}
+            cardTag={"NEW"}
+            thumb={"https://via.placeholder.com/300x300"}
+            alttext={"some alt text"}
+            doctitle={"Sell Sheet Title"}
+            linkCopy={"Download"}
+          />
+
+          <Resource
+            link={"https://google.com"}
+            cardTag={"NEW"}
+            thumb={"https://via.placeholder.com/300x300"}
+            alttext={"some alt text"}
+            doctitle={"Sell Sheet Title"}
+            linkCopy={"Download"}
+          />
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default FlexScroll;
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import Layout from "@/components/Layout";
 import LayoutEditor from "@/components/Drawers/LayoutEditor";
+// Hook into when anyone visits any page on the site
+import { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../app/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    console.log('App loaded');
+  }, []);
   return (
     <>
       <LayoutEditor />
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
-  )
+  );
 }
