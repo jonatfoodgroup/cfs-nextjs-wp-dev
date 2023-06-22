4cacda7cce1b29fd7749a10f3b33298aecf6a406
mcasper78 <121960293+mcasper78@users.noreply.github.com>
2023-06-22 13:17:30 -0400

    card and slick updates.

--- pages/index.js
+++ pages/index.js
@@ -111,6 +111,32 @@ const SegmentCarousel = () => {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 3,
+        responsive: [
+          {
+            breakpoint: 100,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+             
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+             
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+        
       }}
       items={data.segments.nodes}
       title={"SegmentCarousel"}
