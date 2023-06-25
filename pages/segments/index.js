c5002e682b8284c00a2a4494d844eedc706fbb03
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-06-25 14:57:48 -0400

    Segment selection on segments archive

--- pages/segments/index.js
+++ pages/segments/index.js
@@ -3,12 +3,14 @@ import GET_SEGMENTS from "@/app/graphql/queries/GET_SEGMENTS";
 import client from "@/app/apollo-client";
 import PageMeta from "@/components/PageMeta";
 import { Container, Row, Col } from "react-bootstrap";
+import { useState } from "react";
 
 const SegmentsArchive = () => {
   const { loading, error, data } = useQuery(GET_SEGMENTS, {
     client: client,
   });
 
+  const [selectedSegment, setSelectedSegment] = useState(null);
   return (
     <>
       <PageMeta title="Segments" description="Campbell's Foodservice Segments" />
@@ -19,9 +21,7 @@ const SegmentsArchive = () => {
               Get solutions and inspiration for your business segment from Campbell&#8217;s Foodservice.
             </h2>
             <p>
-              {/* Partner with Campbell’s Foodservice and you get a powerful */}
               lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. 
-              
             </p>
           </Col>
           <Col md={{ span: 6 }} className={"mt-3"}>
@@ -33,7 +33,32 @@ const SegmentsArchive = () => {
           </Col>
         </Row>
       </Container>
+
+      <Container>
+        <Row style={{padding: "10vh 0px"}}>
+          <Col md={'4'}>
+            {data?.segments?.nodes?.map((segment) => (
+              <div key={segment.id} onClick={() => setSelectedSegment(segment)}>
+                <h4 style={{fontWeight: 400, color: (selectedSegment?.id === segment.id) ? "#e31837" : "#000"}}>{segment.title}</h4>
+              </div>
+            ))}
+          </Col>
+          <Col md={'8'}>
+            {selectedSegment && (
+              <SegmentTab segment={selectedSegment} />
+            )}
+          </Col>
+        </Row>
+      </Container>
     </>
+  );
+};
+
+const SegmentTab = ({ segment }) => {
+  return (
+    <div>
+      <h4>{segment.title}</h4>
+    </div>
   );
 };
 
