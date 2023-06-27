9fee19f1acbb664f508bda69e673ac5e7a6bfe75
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-06-27 03:02:25 -0400

    update

--- pages/brands/index.js
+++ pages/brands/index.js
@@ -74,6 +74,29 @@ const BrandCarousel = ({ brands, onClick }) => {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+    ],
   };
   return (
     <Slider {...settings}>
