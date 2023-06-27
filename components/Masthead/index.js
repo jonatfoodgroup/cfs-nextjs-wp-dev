967e8a5f5e07e7dcc2ad4342c3bd6f05bcad1a1d
mcasper78 <121960293+mcasper78@users.noreply.github.com>
2023-06-27 14:01:29 -0400

    masthead finalizd -for today

--- components/Masthead/index.js
+++ components/Masthead/index.js
@@ -7,8 +7,8 @@ const Masthead = () => {
   return (
     <section className={styles.masthead} data-aos="fade-in">
       <Row className={styles.homeMasthead}>
-        <Col><img src="https://campbellsf1dev.wpengine.com/wp-content/uploads/2023/06/home-headline-fullwidth.svg" alt="Get more with Campbell's" className={styles.masthead__title} />
-
+        <Col className={styles.homeMasthead__title}>
+          <img src="https://campbellsf1dev.wpengine.com/wp-content/uploads/2023/06/home-headline-fullwidth.svg" alt="Get more with Campbell's" className={styles.masthead__title}  data-aos="fade-up" data-aos-delay="400" class="aos-init aos-animate"/>
         </Col>
       </Row>
     </section>
