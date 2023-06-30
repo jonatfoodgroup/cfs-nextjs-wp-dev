e9a3a9d6c96b02f146e407a011ec23d4dae8da85
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-06-30 08:40:31 -0400

    Added recipes to homepage

--- components/HomeRecipes/index.js
+++ components/HomeRecipes/index.js
@@ -39,8 +39,7 @@ const HomeRecipes = () => {
       <Container>
         <Row className="justify-content-md-center">
           <Col>
-            <div className="RecipeCatLinks centered">
-            </div>
+            <div className="RecipeCatLinks centered"></div>
           </Col>
         </Row>
       </Container>
@@ -49,43 +48,45 @@ const HomeRecipes = () => {
         <Row className="justify-content-md-center">
           <Col md={5}>
             <img
-              src="https://via.placeholder.com/650x650"
+              src="https://www.campbellsfoodservice.com/wp-content/uploads/assets/large/autumn-harvest-salad.jpg"
               className="img-fluid"
             />
-            <h5>Title</h5>
-            <p> description</p>
+              <p style={{marginTop: 10}}>Autmn Harvest Salad Made with Campbell's Signature Harvest Butternut Squash</p>
+              <p style={{fontSize: '.8rem'}}>This autumn harvest salad features butternut squash, dried cherries, candied walnuts, goat cheese, pomegranate seeds and salad greens tossed with a sweet vinaigrette made with Campbell’s® Signature Harvest Butternut Squash soup</p>
           </Col>
           <Col md={5}>
             <Row>
               <Col md={6}>
                 <img
-                  src="https://via.placeholder.com/300x300"
+                  src="https://www.campbellsfoodservice.com/wp-content/uploads/assets/large/Tomato-Soup_Plant_Creamy-Tomato-Basil.jpg"
                   className="img-fluid"
                 />
-                <h5>Title</h5>
+
+                {/* Rewritten in proper casing */}
+                <p style={{fontSize: '.8rem', marginTop: 8}}>“Creamy” Tomato Basil Soup Made with Campbell’s® Condensed Tomato Soup</p>
               </Col>
 
               <Col md={6}>
                 <img
-                  src="https://via.placeholder.com/300x300"
+                  src="https://www.campbellsfoodservice.com/wp-content/uploads/2019/01/sauted-onion-signature-sauteed-onion-mushroom-bisque.jpg"
                   className="img-fluid"
                 />
-                <h5>Title</h5>
+                <p style={{fontSize: '.8rem', marginTop: 8}}>Sauteed Onion & Mushroom Gravy Made with Campbell’s® Signature Sauteed Onion & Mushroom Bisque</p>
               </Col>
               <Col md={6}>
                 <img
-                  src="https://via.placeholder.com/300x300"
+                  src="https://www.campbellsfoodservice.com/wp-content/uploads/assets/large/FoodServiceCreamyTomatoVodkaSauce.jpg"
                   className="img-fluid"
                 />
-                <h5>Title</h5>
+                <p style={{fontSize: '.8rem', marginTop: 8}}>Pasta with Creamy Tomato Vodka Sauce Made</p>
               </Col>
 
               <Col md={6}>
                 <img
-                  src="https://via.placeholder.com/300x300"
+                  src="https://www.campbellsfoodservice.com/wp-content/uploads/assets/large/FoodService_Pace_JerkRubChickenWithWatermelonSalsa.jpg"
                   className="img-fluid"
                 />
-                <h5>Title</h5>
+                <p style={{fontSize: '.8rem', marginTop: 8}}>Jerk Chick With Watermelon Salsa</p>
               </Col>
             </Row>
           </Col>
