22f50ce10281983f6b2aacf98bd6c3c593c64494
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-06-30 11:57:17 -0400

    Push branch

--- components/BrandFeatures/index.js
+++ components/BrandFeatures/index.js
@@ -5,23 +5,22 @@ import client from "@/app/apollo-client";
 import GET_BRAND from "@/app/graphql/queries/GET_BRAND";
 import styles from "./BrandFeatures.module.css";
 
-const BrandFeatures = ({ brandAttribute, brandImage  }) => {
+const BrandFeatures = ({ brandAttribute, brandImage }) => {
   console.log(brandAttribute);
   return (
-      <>
-   <Container className={styles.keyAttribute} >
-      <Row className={"m-3 d-flex align-items-center justify-content-center"}>
-        <Col 
-        md={{ span: 5 }}
-        className={"mt-2"}
-        data-speed="-.2"
+    <>
+      <Container className={styles.keyAttribute}>
+        <Row className={"m-3 d-flex align-items-center justify-content-center"}>
+          <Col
+            md={{ span: 5 }}
+            className={"mt-2"}
+            data-speed="-.2"
             data-aos="fade-right"
             data-aos-duration="750"
             data-aos-once="true"
             data-aos-delay="750"
-            >
-        <p dangerouslySetInnerHTML={{ __html: brandAttribute }}></p>
-
+          >
+            <p dangerouslySetInnerHTML={{ __html: brandAttribute }}></p>
           </Col>
 
           <Col md={6}>
@@ -34,10 +33,10 @@ const BrandFeatures = ({ brandAttribute, brandImage  }) => {
             data-aos-delay="750"
             alt="the featured image here"  /> */}
           </Col>
-      </Row>
-    </Container>
+        </Row>
+      </Container>
     </>
   );
-}
+};
 
 export default BrandFeatures;
