f3acfc3afb47b0a8c47112af2039b980b018b7ef
mcasper78 <121960293+mcasper78@users.noreply.github.com>
2023-07-11 17:09:03 -0400

    image icons for use in the product nav

--- components/Header/Nav/Products/index.js
+++ components/Header/Nav/Products/index.js
@@ -3,6 +3,9 @@ import { useQuery } from "@apollo/client";
 import client from "@/app/apollo-client";
 import { Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import Image from 'next/image';
+import Link from "next/link";
+import styles from "./Products.module.css";
 
 const ProductsMenu = () => {
   const { loading, error, data } = useQuery(GET_PRODUCTS, {
@@ -10,16 +13,146 @@ const ProductsMenu = () => {
   });
   const tabs = [
     {
-      title: "All",
+      title: "Bakery",
       slug: "soups",
-      component: <p>Test</p>
+      component: 
+      <div className={styles.productMenuItem}>
+      <Image
+          src="/product-nav-bakery.png"
+          height={183}
+          width={200}
+          alt="Bakery"
+          quality={100}
+          className="#" />
+        <Link href="#">Bakery</Link>
+      </div>
+
     },
     {
-      title: "Breakfast",
-      slug: "breakfast",
-      component: <p>Test</p>
+      title: "Bases, Broths and Stocks",
+      slug: "soups",
+      component: 
+      <div className={styles.productMenuItem}>
+      <Image
+          src="/product-nav-bases.png"
+          height={183}
+          width={200}
+          quality={100}
+          alt="Bakery"
+          className="#" />
+        <Link href="#">Bases, Broths 
+and Stocks</Link>
+      </div>
+
+    }, 
+    {
+      title: "Beverages",
+      slug: "soups",
+      component: 
+      <div className={styles.productMenuItem}>
+      <Image
+          src="/product-nav-bev.png"
+          height={183}
+          width={200}
+          quality={100}
+          alt="Bakery"
+          className="#" />
+        <Link href="#">Beverages</Link>
+      </div>
+
     },
+    {
+      title: "Entrees",
+      slug: "soups",
+      component: 
+      <div className={styles.productMenuItem}>
+      <Image
+          src="/product-nav-entree.png"
+          height={183}
+          width={200}
+          quality={100}
+          alt="Entrees"
+          className="#" />
+        <Link href="#">Entrees</Link>
+      </div>
+
+    }
+    ,
+    {
+      title: "Sauces, Salsas and Gravies",
+      slug: "soups",
+      component: 
+      <div className={styles.productMenuItem}>
+      <Image
+          src="/product-nav-sauces.png"
+          height={183}
+          width={200}
+          quality={100}
+          alt="Entrees"
+          className="#" />
+        <Link href="#">Sauces, Salsas and Gravies</Link>
+      </div>
+
+    }
+    ,
+    {
+      title: "Snacks",
+      slug: "soups",
+      component: 
+      <div className={styles.productMenuItem}>
+      <Image
+          src="/product-nav-snacks.png"
+          height={183}
+          width={200}
+          quality={100}
+          alt="Entrees"
+          className="#" />
+        <Link href="#">Snacks</Link>
+      </div>
+
+    }
+    ,
+    {
+      title: "Soups",
+      slug: "soups",
+      component: 
+      <div className={styles.productMenuItem}>
+      <Image
+          src="/product-nav-soups.png"
+          height={183}
+          width={200}
+          quality={100}
+          alt="Soups"
+          className="#" />
+        <Link href="#">Soups</Link>
+      </div>
+
+    }
   ];
+{/*
+complete with icons from /Public:
+
+Bakery
+
+Bases, Broths 
+and Stocks
+
+Beverages
+
+Entrees
+
+Sauces, Salsas 
+and Gravies
+
+Snacks
+
+Soups
+
+
+
+*/}
+
+
 
   useEffect(() => {
     console.log('data',);
